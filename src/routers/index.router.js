a0191aaec1f3869951f1534e4fbe25a1ec6e8993
VsalCode <vsal.ilham@gmail.com>
2025-12-12 03:39:19 +0700

    remove old src folder

--- src/routers/index.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const employeeRouter = require("./employee.router");
-
-router.use("/employees", employeeRouter);
-
-module.exports = router;
