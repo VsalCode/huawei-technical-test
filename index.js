ad67245c7140c4c399613628ce076ba4dd0a07ad
VsalCode <vsal.ilham@gmail.com>
2025-12-11 16:51:53 +0700

    setup node js express project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routers = require("./src/routers")
+
+const app = express();
+
+app.use(express.json());
+app.use("/", routers)
+
+app.get("/", (req, res) => {
+  res.send("Express server is running!");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ src/routers/index.router.js
@@ -0,0 +1,5 @@
+const routers = require("express").Router()
+
+routers.use("/")
+
+module.exports = routers;
